f7c409253fa131917a80fd47e6c780ef608bddba
Krzysztof Krystian Jankowski <kj@p1x.in>
2026-04-03 23:57:10 +0200

    1.0-alpha

---
+++ src/main.zig
@@ -0,0 +1,112 @@
+// *************************************
+// P1X ZIG ENGINE
+// by Krzysztof Krystian Jankowski
+// github.com/w84death/p1x-zig-engine
+// *************************************
+
+const std = @import("std");
+const c = @cImport({
+    @cInclude("fenster.h");
+    @cInclude("fenster_audio.h");
+});
+const CONF = @import("config.zig").CONF;
+const PIVOTS = @import("fui.zig").PIVOTS;
+const StateMachine = @import("state.zig").StateMachine;
+const State = @import("state.zig").State;
+const Fui = @import("fui.zig").Fui;
+const Vec2 = @import("math.zig").Vec2;
+const Mouse = @import("math.zig").Mouse;
+const MenuScene = @import("scenes/menu.zig").MenuScene;
+const AboutScene = @import("scenes/about.zig").AboutScene;
+
+pub fn main() void {
+    var buf: [CONF.SCREEN_W * CONF.SCREEN_H]u32 = undefined;
+    var f = std.mem.zeroInit(c.fenster, .{
+        .width = CONF.SCREEN_W,
+        .height = CONF.SCREEN_H,
+        .title = CONF.THE_NAME,
+        .buf = &buf[0],
+    });
+    _ = c.fenster_open(&f);
+    defer c.fenster_close(&f);
+    var mouse_pressed = false;
+    var mouse_lock = false;
+    var right_mouse_pressed = false;
+    var right_mouse_lock = false;
+    var fui = Fui.init(&buf);
+    var sm = StateMachine.init(State.main_menu);
+
+    var menu = MenuScene.init(fui, &sm);
+    var about = AboutScene.init(fui, &sm);
+
+    var close_application = false;
+    var dt: f32 = 0.0;
+    var now: i64 = c.fenster_time();
+
+    while (!close_application and c.fenster_loop(&f) == 0) {
+        const d: f32 = @floatFromInt(c.fenster_time() - now);
+        dt = @as(f32, d / 1000.0);
+        now = c.fenster_time();
+        sm.update();
+        fui.clear_background(CONF.COLOR_BG);
+
+        if (mouse_lock and mouse_pressed and (f.mouse & 1) == 0) {
+            mouse_pressed = false;
+            mouse_lock = false;
+        } else if (!mouse_lock and !mouse_pressed and (f.mouse & 1) != 0) {
+            mouse_pressed = true;
+            mouse_lock = true;
+        } else if (mouse_lock and !mouse_pressed and (f.mouse & 1) == 0) {
+            mouse_pressed = false;
+            mouse_lock = false;
+        } else {
+            mouse_pressed = false;
+        }
+
+        if (right_mouse_lock and right_mouse_pressed and (f.mouse & 2) == 0) {
+            right_mouse_pressed = false;
+            right_mouse_lock = false;
+        } else if (!right_mouse_lock and !right_mouse_pressed and (f.mouse & 2) != 0) {
+            right_mouse_pressed = true;
+            right_mouse_lock = true;
+        } else if (right_mouse_lock and !right_mouse_pressed and (f.mouse & 2) == 0) {
+            right_mouse_pressed = false;
+            right_mouse_lock = false;
+        } else {
+            right_mouse_pressed = false;
+        }
+        const mouse = Mouse.init(f.x, f.y, mouse_pressed, right_mouse_pressed);
+
+        switch (sm.current) {
+            State.main_menu => {
+                menu.draw(mouse);
+            },
+            State.about => {
+                about.draw(mouse);
+            },
+            State.quit => {
+                close_application = true;
+            },
+        }
+
+        if (f.keys[27] != 0) {
+            break;
+        }
+
+        // Quit
+        if (!sm.is(State.main_menu) and fui.button(fui.pivots[PIVOTS.TOP_RIGHT].x - 80, fui.pivots[PIVOTS.TOP_RIGHT].y, 80, 32, "Quit", CONF.COLOR_MENU_NORMAL, mouse)) {
+            sm.goTo(State.quit);
+        }
+        // Version
+        fui.draw_version();
+
+        fui.draw_cursor_lines(Vec2.init(f.x, f.y));
+
+        const frame_time_target: f64 = 1000.0 / 30.0;
+        const processing_time: f64 = @floatFromInt(c.fenster_time() - now);
+        const sleep_ms: i64 = @intFromFloat(@max(0.0, frame_time_target - processing_time));
+        if (sleep_ms > 0) {
+            c.fenster_sleep(sleep_ms);
+        }
+    }
+}
---
+++ src/scenes/about.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+const CONF = @import("../config.zig").CONF;
+const Fui = @import("../fui.zig").Fui;
+const PIVOTS = @import("../fui.zig").PIVOTS;
+const State = @import("../state.zig").State;
+const StateMachine = @import("../state.zig").StateMachine;
+const Vec2 = @import("../math.zig").Vec2;
+const Mouse = @import("../math.zig").Mouse;
+
+pub const AboutScene = struct {
+    fui: Fui,
+    sm: *StateMachine,
+    pub fn init(fui: Fui, sm: *StateMachine) AboutScene {
+        return AboutScene{ .fui = fui, .sm = sm };
+    }
+    pub fn draw(self: *AboutScene, mouse: Mouse) void {
+        const px = self.fui.pivots[PIVOTS.TOP_LEFT].x;
+        const py = self.fui.pivots[PIVOTS.TOP_LEFT].y;
+        if (self.fui.button(px, py, 120, 32, "< Menu", CONF.COLOR_MENU_SECONDARY, mouse)) {
+            self.sm.goTo(State.main_menu);
+        }
+
+        var ay: i32 = py + 64;
+        const lines = [_][:0]const u8{
+            "P1X ZE is an Zig Engine for creating small",
+            "applications for Linux and Windows.",
+            "Uses customized fenster CPU renderer.",
+            "",
+            "",
+            "Source code available at:",
+            "https://github.com/w84death/p1x-zig-engine",
+            "",
+            "MIT Licence.",
+        };
+
+        const line_height = 24;
+        for (lines) |line| {
+            self.fui.draw_text(line, px, ay, CONF.FONT_DEFAULT_SIZE, CONF.COLOR_PRIMARY);
+            ay += line_height;
+        }
+    }
+};
