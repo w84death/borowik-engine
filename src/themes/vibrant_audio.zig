0743100f74ef47e9a42f4777f1c9ab70566b960a
Krzysztof Krystian Jankowski <kj@p1x.in>
2026-04-06 14:45:52 +0200

    revert

---
+++ src/themes/vibrant_audio.zig
@@ -0,0 +1,40 @@
+// *************************************
+// BOROWIK ENGINE
+// by Krzysztof Krystian Jankowski
+// github.com/w84death/borowik-engine
+// *************************************
+
+const A = @import("../engine/audio.zig");
+
+pub const AudioTheme = struct {
+    pub const SFX_MENU_MAIN = [_]A.Note{
+        .{ .id = A.NOTE_C5, .dur = 0.03 },
+        .{ .id = A.NOTE_E5, .dur = 0.03 },
+        .{ .id = A.NOTE_G5, .dur = 0.03 },
+        .{ .id = A.NOTE_C6, .dur = 0.05 },
+    };
+
+    pub const SFX_MENU_BACK = [_]A.Note{
+        .{ .id = A.NOTE_E5, .dur = 0.03 },
+        .{ .id = A.NOTE_C5, .dur = 0.03 },
+        .{ .id = A.NOTE_A4, .dur = 0.04 },
+    };
+
+    pub const SFX_POPUP = [_]A.Note{
+        .{ .id = A.NOTE_G5, .dur = 0.025 },
+        .{ .id = A.NOTE_B5, .dur = 0.025 },
+        .{ .id = A.NOTE_D6, .dur = 0.03 },
+        .{ .id = A.NOTE_G6, .dur = 0.05 },
+    };
+
+    pub const SFX_EXPLOSION = [_]A.Note{
+        .{ .id = A.NOTE_C6, .dur = 0.02 },
+        .{ .id = A.NOTE_E6, .dur = 0.02 },
+        .{ .id = A.NOTE_G6, .dur = 0.02 },
+        .{ .id = A.NOTE_B6, .dur = 0.02 },
+        .{ .id = A.NOTE_A6, .dur = 0.02 },
+        .{ .id = A.NOTE_F6, .dur = 0.02 },
+        .{ .id = A.NOTE_D6, .dur = 0.02 },
+        .{ .id = A.NOTE_C7, .dur = 0.03 },
+    };
+};
